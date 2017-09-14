3ad9d144cc9d3f82df511716a1569166eb94badc
marcw24 <marcw24@uw.edu>
2017-09-14 07:59:42 -0700

    added ckeditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import InlineEditor from '@ckeditor/ckeditor5-build-inline/build/ckeditor.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    InlineEditor.create(document.querySelector('#editor')).then( editor => {
+    });
+  }
 }
