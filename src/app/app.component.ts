7c68980e5e8de4654222c124b394de8adb8dfe4b
Marc Wilson <marc@headset.io>
2018-01-10 11:19:45 -0800

    created example for multiple ckeditors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import InlineEditor from '@ckeditor/ckeditor5-build-inline/build/ckeditor.js';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic/build/ckeditor';
+import * as uniqid from 'uniqid';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  public instanceCount = 1;
+  public ckeditorIds: string[] = [];
 
   ngOnInit(): void {
-    InlineEditor.create(document.querySelector('#editor')).then( editor => {
-    });
+    this.ckeditorIds.push(uniqid());
+  }
+  createInstances(): void {
+    for (let i = 0; i < this.instanceCount; i++) {
+      ClassicEditor
+        .create( document.querySelector( `#my-ckeditor-instance-${this.ckeditorIds[i]}` ) )
+        .then( editor => {
+          // TODO: Do something with the editor
+        } )
+        .catch( error => {
+          console.error( error );
+        } );
+    }
+  }
+  createIds(): string[] {
+    this.ckeditorIds = [];
+    for (let i = 0; i < this.instanceCount; i++) {
+      this.ckeditorIds.push(uniqid());
+    }
+    return this.ckeditorIds;
   }
 }
